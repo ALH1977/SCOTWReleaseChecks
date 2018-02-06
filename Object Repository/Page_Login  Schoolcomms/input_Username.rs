e3dfd69a846f4bdc3987e47fb757d898b002f2c4
ALH1977 <36156921+ALH1977@users.noreply.github.com>
2018-02-06 10:55:14 +0000

    Add files via upload

---
+++ Object Repository/Page_Login  Schoolcomms/input_Username.rs
@@ -0,0 +1,51 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>input_Username</name>
+   <tag></tag>
+   <elementGuidId>30e08883-2ce4-4024-a434-5db8fa30cb01</elementGuidId>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>input</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>id</name>
+      <type>Main</type>
+      <value>username</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>name</name>
+      <type>Main</type>
+      <value>Username</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>placeholder</name>
+      <type>Main</type>
+      <value>Enter your username</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>type</name>
+      <type>Main</type>
+      <value>text</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;username&quot;)</value>
+   </webElementProperties>
+</WebElementEntity>
---
+++ Test Suites/Release Checks.ts
@@ -0,0 +1,56 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Release Checks</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <lastRun>2018-02-05T15:16:20</lastRun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>5a8b98a6-b655-49e1-b5c8-db67a6c39efe</testSuiteGuid>
+   <testCaseLink>
+      <guid>f49e0231-7067-4dd4-88b4-24ff855207f5</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/HTTPS Redirect - Not Logged In - LIVE</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>ec8e3a5f-9524-4583-a9e7-323abebee6a4</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/HTTPS Redirect - Logged In - Labs</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>3ddb662e-b668-4e51-ade0-3bae36208764</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Error Page</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>0043ba13-246d-40a1-88ab-148abc8f6457</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/HTTPS Redirect - Not Logged In - Labs</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>74fe7076-a008-4eef-9f7e-4489e88c7ade</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Remember Me</testCaseId>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>124180ac-7140-4a5c-958b-cf751c95d681</variableId>
+      </variableLink>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>2ebab4d4-6740-4c21-a6f5-2cfda42b2543</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/HTTPS Redirect - Logged In - LIVE</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
